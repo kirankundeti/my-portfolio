aaf6d91478d00a147566bcd843a9d3328a08ab79
kiran kundeti <kirankundeti08@gmail.com>
2024-02-17 22:22:51 +0530

    Added portfolio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.typedtext', {
+    strings: ['Developer', 'Editor', 'Designer', 'Coder', 'Photographer'],
+    typeSpeed: 50,
+    loop: true,
+    backDelay:1500,
+    backSpeed : 50,
+  });
+
+  var typed = new Typed('.typedata', {
+    strings: ['Get in Touch', 'Contact Information', "Let's Chat"],
+    typeSpeed: 50,
+    loop: true,
+    backDelay:1500,
+    backSpeed : 50,
+  });
